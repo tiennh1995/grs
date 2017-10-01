f20f738b94328a8ccec98fc0816e2780a74a4d5e
Hoang Tung Lam <hoanglam995@gmail.com>
2017-10-01 07:10:31 +0700

    Sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:nick_name, :avatar, :email, :password,
+        :password_confirmation)
+    end
+  end
 end
