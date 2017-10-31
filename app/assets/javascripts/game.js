5d6338475dc372d406e2512ebf59281668708e4f
Nguyen Huu Tien <tiennh1995@gmail.com>
2017-10-31 14:59:13 +0700

    fix review when zero

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,10 +1,14 @@
 document.addEventListener('turbolinks:load', function() {
   var sum = $('.game-sum-review span').prop('textContent');
-  var rateFive = $('.bar-5 span').prop('textContent') / sum * 80;
-  var rateFour = $('.bar-4 span').prop('textContent') / sum * 80;
-  var rateThree = $('.bar-3 span').prop('textContent') / sum * 80;
-  var rateTwo = $('.bar-2 span').prop('textContent') / sum * 80;
-  var rateOne = $('.bar-1 span').prop('textContent') / sum * 80;
+  if(sum == 0) {
+    rateFive = rateFour = rateThree = rateTwo = rateOne = 0;
+  } else {
+    rateFive = $('.bar-5 span').prop('textContent') / sum * 80;
+    rateFour = $('.bar-4 span').prop('textContent') / sum * 80;
+    rateThree = $('.bar-3 span').prop('textContent') / sum * 80;
+    rateTwo = $('.bar-2 span').prop('textContent') / sum * 80;
+    rateOne = $('.bar-1 span').prop('textContent') / sum * 80;
+  }
   $('.bar-5').css('width', rateFive + '%');
   $('.bar-4').css('width', rateFour + '%');
   $('.bar-3').css('width', rateThree + '%');
