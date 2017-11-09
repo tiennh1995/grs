a2cb67d26888e065e82e844d0adde4eca7034cf3
Nguyen Huu Tien <tiennh1995@gmail.com>
2017-11-09 22:55:46 +0700

    admin list request

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,10 +1,10 @@
 class Admin::AdminController < ApplicationController
   layout "admin/application"
 
-  before_action :authorize_user
+  before_action :authenticate_user!, :authorize_user
 
   protected
   def authorize_user
-    redirect_to root_path unless current_user && current_user.admin?
+    redirect_to root_path unless current_user.admin?
   end
 end
