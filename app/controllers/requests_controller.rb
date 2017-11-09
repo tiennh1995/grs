f43d78c583ee30d3f40d4845f4073aa823528266
Nguyen Huu Tien <tiennh1995@gmail.com>
2017-11-09 22:17:07 +0700

    destroy request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   before_action :authenticate_user!
+  before_action :load_request, only: :destroy
 
   def index
     status = params[:status]
@@ -19,8 +20,20 @@ class RequestsController < ApplicationController
     end
   end
 
+  def destroy
+    @request.destroy
+  end
+
   private
   def request_params
     params.require(:request).permit :game_name, :genre_info, :required, :info
+  end
+
+  def load_request
+    @request = Request.find_by id: params[:id]
+    unless @request
+      flash[:danger] = "Request not exist!"
+      redirect_to root_path
+    end
   end
 end
