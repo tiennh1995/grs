43d166914eaad45f1a7cc71b543d42dfe76cb544
Nguyen Huu Tien <tiennh1995@gmail.com>
2017-11-14 15:20:31 +0700

    admin check request

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,7 +1,28 @@
 class Admin::RequestsController < Admin::AdminController
+  before_action :load_request, only: :update
+
   def index
     status = params[:status]
     status ||= "all"
     @requests = Request.send(status).includes(:user).page(params[:page]).per 9
+  end
+
+  def update
+    if Request.statuses.include? request_params[:status]
+      @request.update_attributes request_params
+    end
+  end
+
+  private
+  def request_params
+    params.require(:request).permit :status
+  end
+
+  def load_request
+    @request = Request.find_by id: params[:id]
+    unless @request
+      flash[:danger] = "Request not exist!"
+      redirect_to admin_requests_path
+    end
   end
 end
