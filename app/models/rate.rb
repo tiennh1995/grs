57b3709c43d1eb68bfef4d2f65101738b794ec78
Nguyen Huu Tien <tiennh1995@gmail.com>
2017-11-23 12:47:56 +0700

    limit review game

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -6,4 +6,10 @@ class Rate < ApplicationRecord
   validates :game, presence: true
   validates :point, presence: true,
     numericality: {greater_than: 0, less_than_or_equal_to: 5}
+
+  after_save :update_game_point
+
+  def update_game_point
+    game.update_attributes point: game.avarage_rate
+  end
 end
